b5d3cc7ffbbcb5c5d9354ded598de133770aa035
George Armando <georgearmando54@gmail.com>
2020-07-15 03:05:41 +0100

    Criação das rotas para listagem, criação, actualização, remoção e likes

--- src/app.js
+++ src/app.js
@@ -11,13 +11,13 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs, likes } = request.body
 
-  const repository = { id: uuid(), title, url, techs, likes}
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
 
   repositories.push(repository)
 
@@ -26,15 +26,66 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(rep => rep.id == id)
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: 'Repository not found' })
+  }
+
+  const { likes } = repositories[repositoryIndex]
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes 
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(rep => rep.id == id)
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: 'Repository not found!' })
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(rep => rep.id == id)
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error:'Repository not found!' })
+  }
+
+  const { title, url, techs, likes } = repositories[repositoryIndex]
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: likes + 1
+  }  
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 module.exports = app;
